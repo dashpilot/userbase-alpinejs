410d2add91c30e33eeac58a336868cb7478cb937
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-01-10 05:44:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,100 @@
+function app(messageDelay) {
+  const userbaseReady = false;
+  const username = "";
+  const password = "";
+  const loggedIn = false;
+  const email = "";
+  const profile = null;
+  const message = "";
+  const showSignIn = false;
+  const isError = false;
+  const loading = false;
+
+  return {
+    userbaseReady: userbaseReady,
+    username: username,
+    password: password,
+    email: email,
+    profile: profile,
+    message: "",
+    showSignIn: true,
+    loggedIn: loggedIn,
+    isError: isError,
+    loading: loading,
+
+    msg(msg) {
+      this.message = msg
+      setTimeout(() => {
+        this.message = null
+      }, messageDelay)
+    },
+
+    init() {
+      userbase
+        .init({
+          appId: "a9b0e582-9b46-4a57-ad10-1fe7f877d699"
+        })
+        .then(session => {
+          this.userbaseReady = true;
+
+          if (session.user) {
+            this.username = session.user.username;
+            this.email = session.user.email;
+            this.profile = session.user.profile;
+            this.loggedIn = true;
+            this.isError = false;
+            console.log(this.username + " is logged in");
+          } else {
+            this.loggedIn = false;
+          }
+        });
+    },
+
+    signUp(username, password) {
+      this.loading = true;
+      userbase
+        .signUp({
+          username,
+          password
+        })
+        .then(user => {
+          this.msg("Account created for " + username)
+          this.loggedIn = true;
+          this.isError = false;
+          this.loading = false;
+        })
+        .catch(err => {
+          this.msg("Error: Could not create account")
+          this.isError = true;
+          this.loading = false;
+        });
+    },
+
+    signIn(username, password) {
+      this.loading = true;
+      userbase
+        .signIn({
+          username,
+          password
+        })
+        .then(user => {
+          this.msg(username + " logged in")
+          this.loggedIn = true;
+          this.isError = false;
+          this.loading = false;
+        })
+        .catch(err => {
+          this.msg("Error: username or password incorrect")
+          this.isError = true;
+          this.loading = false;
+        });
+    },
+
+    signOut() {
+      userbase.signOut().then(user => {
+        this.msg("Signed out")
+        this.loggedIn = false;
+      });
+    }
+  };
+}
